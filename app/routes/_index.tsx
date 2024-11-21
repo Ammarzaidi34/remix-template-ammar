114c9fc3414cfacc894e8668e0d99ff6428e83a8
Ammarzaidi34 <ammar.zaidi@zuppler.com>
2024-11-21 12:51:41 +0530

    added brand variable

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Auto Genegate Website" }
+    { title: "Auto Genegrate Website" }
   ];
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: "#4f46e5"
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
